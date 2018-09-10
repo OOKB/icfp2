75db5848cb6840e902f625fdd31eb8281a0f47e8
Kristian Bjornard <kb@ookb.co>
2018-09-10 00:13:04 -0400

    attempting to create mulitple lines of included text for each "section"

--- src/Session.jsx
+++ src/Session.jsx
@@ -3,12 +3,16 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
 import css from 'cape-style'
 
 // Create styles
-const styles = StyleSheet.create(css('bgWashedBlue'))
+const styles = StyleSheet.create(css('bb'))
 
 // Create Document Component
 function Session({SessionName, SessionStartTime, SessionType}) {
   return (
-    <Text>{SessionStartTime}, {SessionName}, {SessionType}</Text>
+    <View style={styles} wrap>
+      <Text>{SessionStartTime}</Text>
+      <Text>{SessionName}</Text>
+      <Text>{SessionType}</Text>
+    </View>
   )
 }
 
