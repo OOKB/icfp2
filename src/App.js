e36ab3f4f039e09be4ce467a500c717ef43e2b0b
Kristian Bjornard <kb@ookb.co>
2018-09-11 10:45:44 -0400

    getting sort of somewhere with the layout, attempting font?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
 import data from './data'
 import Session from './Session'
+
+Font.register(
+  './UnifrakturCook-Bold.ttf',
+  { family: 'Blackletter' },
+);
 
 const styles = StyleSheet.create({
   page: {
     paddingTop: '36',
     paddingRight: '54',
     paddingBottom: '90',
-    paddingLeft: '54'
+    paddingLeft: '54',
+    fontFamily: 'Blackletter'
   },
   section: {
     fontFamily: 'Helvetica',
-    fontSize: '10'
+    fontSize: '10',
+    flexDirection: 'column',
+    width: '100%',
   }
 });
 
@@ -20,7 +28,7 @@ const styles = StyleSheet.create({
 const MyDocument = () => (
   <Document>
     <Page size="A4" style={styles.page} wrap>
-      <View style={styles.section} wrap>
+      <View style={styles.section}>
         {data.map((item, i) => <Session key={i} {...item} />)}
       </View>
     </Page>
--- src/Session.jsx
+++ src/Session.jsx
@@ -3,16 +3,35 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
 import css from 'cape-style'
 
 // Create styles
-const styles = StyleSheet.create(css('bt'))
+const styles = StyleSheet.create({
+  row: {
+    borderTopWidth: 1,
+    borderTopStyle: 'solid',
+    borderTopColor: 'gray',
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
+  },
+  column: {
+    flexDirection: 'column',
+    width: '50%'
+  }
+});
 
 // Create Document Component
 function Session({SessionID, SessionName, SessionStartTime, SessionType}) {
   return (
-    <View style={styles} wrap>
-      <Text style={{marginTop: 10}}>ID: {SessionID}</Text>
-      <Text>Date: {SessionStartTime}</Text>
-      <Text>Session Title: {SessionName}</Text>
-      <Text style={{marginBottom: 10}}>Type: {SessionType}</Text>
+    <View style={styles.row} >
+      <View style={styles.column} >
+        <Text>ID: {SessionID}</Text>
+        <Text>Date: {SessionStartTime}</Text>
+        <Text>Type: {SessionType}</Text>
+      </View>
+      <View style={styles.column} >
+        <Text>Session Title: {SessionName}</Text>
+      </View>
     </View>
   )
 }
