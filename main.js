8a0fd22d82c07bff8e0646f66a138b189194204a
Kai Curry <kai@cape.io>
2018-09-18 16:37:13 -0400

    data process

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ import _ from 'lodash/fp'
 import axios from 'axios'
 import qs from 'qs'
 import { writeJson } from 'fs-extra'
+import fixData from './data'
 
 const url = 'https://api.xcdsystem.com/session_json/'
 const form = {
-  apikey: '030437B8-F147-4F1A-9B36-4FB0J6CC9CBE',
+  apikey: process.env.XCD_API_KEY,
   ConferenceID: '696',
   FieldIDs: '8235,8242,8236,10113,8229,8239',
   AllAuthors: '1',
@@ -21,6 +22,10 @@ const options = {
 
 axios(options)
   .then(_.get('data'))
-  .then(data => writeJson('data.json', data, { spaces: 2 }))
+  .then(fixData)
+  .then(({ data, apiData }) => Promise.all([
+    writeJson('data-raw.json', data, { spaces: 2 }),
+    writeJson('data.json', apiData, { spaces: 2 }),
+  ]))
   .then(console.log)
   .catch(console.error)
