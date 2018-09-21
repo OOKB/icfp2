e8d9aec6dbaa134e5111965d317fc150dabaa8e3
Kai Curry <kai@cape.io>
2018-09-20 21:22:17 -0400

    sessioncode

--- src/SessionsList.js
+++ src/SessionsList.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
 
-class SessionsList extends Component {
-  render() {
-    const {
-      sessionCode, sessionName, sessionRoom, sessionStartTime, sessionEndTime,
-    } = this.props
-    const timeStr = `${sessionStartTime} - ${sessionEndTime}`
-    const separator = '\u0009'
+function SessionsList(props) {
+  const {
+    sessionCode, sessionName, sessionRoom, sessionStartTime, sessionEndTime,
+  } = props
+  const timeStr = `${sessionStartTime} - ${sessionEndTime}`
+  const separator = '\u0009'
 
-    return (
-      <div className="item presentation">
-        { separator }
-        <SessionCode>{ sessionCode }</SessionCode>
-        { separator }
-        <sessionname><strong>{ sessionName }</strong></sessionname>
-        { separator }
-        <sessionroom>{ sessionRoom }</sessionroom>
-        { separator }
-      </div>
-    )
-  }
+  return (
+    <div className="item presentation">
+      { separator }
+      <sessioncode>{ sessionCode }</sessioncode>
+      { separator }
+      <sessionname><strong>{ sessionName }</strong></sessionname>
+      { separator }
+      <sessionroom>{ sessionRoom }</sessionroom>
+      { separator }
+    </div>
+  )
 }
 
 export default SessionsList
