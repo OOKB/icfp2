bb53c4c68fda46f531cd3cd069b6a01bf73fd24c
Kristian Bjornard <kb@ookb.co>
2018-09-21 02:08:09 -0400

    okay, I can see the opening things now too

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import json from './data'
 
 // Create Document Component
 const App = () => (
+  <Apps items={json.opening} type="sessions" />
+  // <Apps items={json.workshop} type="sessions" />
   // <Apps items={json.sessions} type="sessions" />
-  <Apps items={json.workshop} type="sessions" />
   // <Apps items={json.posters} type="posters" />
 )
 
