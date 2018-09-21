5953a88c7e9625ff0834ca51f2bac9caa828dd7a
Kai Curry <kai@cape.io>
2018-09-21 15:05:14 -0400

    make Apps simple function

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import Poster from './Poster'
 import SessionDay from './SessionDay'
 import SessionDayReview from './SessionDayReview'
 
-class Apps extends Component {
-  render() {
-    const { items, type } = this.props
-    let ItemTemplate = SessionDay
-    let keyFieldId = 'sessionDate'
+function Apps({ items, type }) {
+  let ItemTemplate = SessionDay
+  let keyFieldId = 'sessionDate'
 
-    if (type === 'posters') {
-      ItemTemplate = Poster
-      keyFieldId = 'sessionID'
-    } else if (type === 'other') {
-      ItemTemplate = SessionDayReview
-    }
-
-    return (
-      <div id={`type-${type}`}>
-        { items.map(item => <ItemTemplate key={item[keyFieldId]} {...item} />) }
-      </div>
-    )
+  if (type === 'posters') {
+    ItemTemplate = Poster
+    keyFieldId = 'sessionID'
+  } else if (type === 'other') {
+    ItemTemplate = SessionDayReview
   }
+
+  return (
+    <div id={`type-${type}`}>
+      { items.map(item => <ItemTemplate key={item[keyFieldId]} {...item} />) }
+    </div>
+  )
 }
+
 Apps.propTypes = {
   items: PropTypes.array.isRequired,
   type: PropTypes.string.isRequired,
