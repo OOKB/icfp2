54bfab00f742857159ab881e748c2d4b247575af
Kai Curry <kai@cape.io>
2018-10-03 23:49:08 -0400

    fix #20

--- src/Poster.jsx
+++ src/Poster.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { startEnd } from './clientUtils'
 
 import Presentation from './Presentation'
 
@@ -8,14 +9,13 @@ class Poster extends Component {
     const {
       sessionName, sessionStartTime, sessionEndTime, sessionRoom, presentations,
     } = this.props
-    const sessionTime = `${sessionStartTime}-${sessionEndTime}`
 
     return (
       <div className="poster-session">
 
         {/* 1 set of meta-data listings per poster-session */}
         <div className="sessionName">{sessionName}</div>
-        <div className="sessionTime">{sessionTime}</div>
+        <div className="sessionTime">{startEnd(sessionStartTime, sessionEndTime)}</div>
         <div className="sessionRoom">{sessionRoom}</div>
         {/* <!-- day? --> */}
 
